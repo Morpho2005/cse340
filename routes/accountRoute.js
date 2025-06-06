4522138f681018479ce60c5b5a17f1223d36d1a1
Morpho2005 <digitalchristian05@gmail.com>
2025-06-06 11:31:42 -0600

    added server-side validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController.js");
 const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validator')
 
 // Route to build login view
 router.get("/login", accountController.buildLogin);
@@ -10,6 +11,11 @@ router.get("/login", accountController.buildLogin);
 // Route to build sing up view
 router.get("/signUp", accountController.buildSignUp)
 
-router.post("/signUp", utilities.handleErrors(accountController.signUpAccount))
+router.post(
+    "/signUp",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.signUpAccount)
+)
 
 module.exports = router;

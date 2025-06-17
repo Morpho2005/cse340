6acfb06575e58d1b15b6ecfe89665b5b3032eab2
Morpho2005 <digitalchristian05@gmail.com>
2025-06-17 12:53:00 -0600

    fixed area where i forgot to remove getnav

--- server.js
+++ server.js
@@ -79,14 +79,12 @@ app.use(async (req, res, next) => {
 * Place after all other middleware
 *************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
   const tool = utilities.getTools(req, res)
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
   if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message,
-    nav,
     tool
   })
 })
